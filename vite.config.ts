58df4c9e3a1e275ca69c94179841c26c035e1f41
liralgazi <liralgazi@gmail.com>
2024-03-16 22:48:39 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -6,3 +6,21 @@ export default defineConfig({
   plugins: [react()],
   server: {proxy: {"/api": "http://localhost:3002"}},
 })
+
+/*
+https://www.youtube.com/watch?v=otaQKODEUFs
+https://www.youtube.com/watch?v=VN27pdF3a6c
+
+npm install typescript @types/node
+npx tsc --init
+npm install express @types/express
+npm install --save-dev @babel/core @babel/preset-env @babel/plugin-proposal-decorators
+npm install socket.io @types/socket.io
+npx ts-node server.ts
+npm install bcrypt cors
+npm install @types/cors
+npm install bcrypt cors dotenv @types/bcrypt @types/cors @types/dotenv
+npm install nodemon --save-dev
+npm install pg
+
+*/
