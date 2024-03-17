0ac9f085be3af121604366bc53ae7c73e50c0c13
liralgazi <liralgazi@gmail.com>
2024-03-17 23:59:13 +0200

    /

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -2,14 +2,15 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
-import MessageBox from "./MessageBox"; // Import MessageBox
-import { Message } from "./Message"; // Import the Message type
+import MessageBox from "./MessageBox";
+import { Message } from "./Message";
 
 const Chat = () => {
   const location = useLocation();
-  const name = location.state.name; // The name passed through route state
+  const name = location.state.name;
   const [messages, setMessages] = useState<Message[]>([]);
 
+  //fetching the data from the db
   useEffect(() => {
     const fetchMessages = async () => {
       try {
@@ -17,7 +18,7 @@ const Chat = () => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data: Message[] = await response.json(); // Type assertion
+        const data: Message[] = await response.json();
         setMessages(data);
       } catch (error) {
         console.error("Error fetching messages:", error);
--- src/pages/Message.ts
+++ src/pages/Message.ts
@@ -3,6 +3,6 @@ export interface Message {
     id: number;
     text: string;
     sender: string;
-    timestamp: Date; // or string, depending on how the date is being fetched
+    timestamp: Date; 
   }
   
