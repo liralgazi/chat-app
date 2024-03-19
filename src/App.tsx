001140d29f7556081e3793329220ba32c0f57897
liralgazi <liralgazi@gmail.com>
2024-03-19 22:13:10 +0200

    /
    adding hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chat from "./pages/chat/Chat";
 import Register from "./pages/register/Register";
 import { Box } from "@mui/material";
-import { useEffect } from "react";
+import useDarkMode from "./components/hooks/useDarkMode.ts";
 
 function App() {
-  useEffect(() => {
-    const updateTheme = () => {
-      const prefersDarkMode = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches;
+  useDarkMode();
 
-      if (prefersDarkMode) {
-        document.body.classList.add("dark-mode");
-        console.log("dark mode");
-      } else {
-        document.body.classList.remove("dark-mode");
-        console.log("light mode");
-      }
-    };
-
-    // Call it to set the initial theme
-    updateTheme();
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      // Listen for changes
-      .addEventListener("change", updateTheme);
-
-    return () => {
-      window
-        .matchMedia("(prefers-color-scheme: dark)")
-        .removeEventListener("change", updateTheme);
-    };
-  }, []);
   return (
     <Box sx={{ height: "100vh", width: "100%" }}>
       <BrowserRouter>
@@ -45,5 +19,4 @@ function App() {
     </Box>
   );
 }
-
 export default App;
---
+++ src/components/hooks/useDarkMode.ts.ts
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+const useDarkMode = () => {
+  useEffect(() => {
+    const updateTheme = () => {
+      const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+      if (prefersDarkMode) {
+        document.body.classList.add("dark-mode");
+        console.log("dark mode");
+      } else {
+        document.body.classList.remove("dark-mode");
+        console.log("light mode");
+      }
+    };
+
+    // Call it to set the initial theme
+    updateTheme();
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      // Listen for changes
+      .addEventListener("change", updateTheme);
+
+    return () => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("change", updateTheme);
+    };
+  }, []);
+};
+
+export default useDarkMode;
