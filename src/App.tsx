40d0de49b47e43b1a04af9055d3f03a7b0e5fbb2
liralgazi <liralgazi@gmail.com>
2024-03-19 22:01:00 +0200

    dark/light theme
    supported in microsoft edge only

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,37 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chat from "./pages/chat/Chat";
 import Register from "./pages/register/Register";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const updateTheme = () => {
+      const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+
+      if (prefersDarkMode) {
+        document.body.classList.add("dark-mode");
+        console.log("dark mode");
+      } else {
+        document.body.classList.remove("dark-mode");
+        console.log("light mode");
+      }
+    };
+
+    // Call it to set the initial theme
+    updateTheme();
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      // Listen for changes
+      .addEventListener("change", updateTheme);
+
+    return () => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("change", updateTheme);
+    };
+  }, []);
   return (
     <Box sx={{ height: "100vh", width: "100%" }}>
       <BrowserRouter>
